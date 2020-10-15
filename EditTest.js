f68fc4f0225963fe8214742878cc0c03a40bcaab
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2020-10-15 21:50:33 +0300

    test auth

--- EditTest.js
+++ EditTest.js
@@ -1,7 +1,12 @@
 var mInputTex = document.getElementById('input_tex');
+mInputTex.disabled = true;
 var mOutputTex = document.getElementById('output_tex');
 var mInputAnswer = document.getElementById('input_answer');
+mInputAnswer.disabled = true;
 var mOutputTexAnswer = document.getElementById('output_tex_answer');
+
+var mSaveButton = document.getElementById('save_button');
+mSaveButton.disabled = true;
 
 var select = document.getElementById('select_number');
 
@@ -21,12 +26,14 @@ function loadNumber(){
   if (sel==0){
     mInputTex.disabled = true;
     mInputAnswer.disabled = true;
+    mSaveButton.disabled = true;
     mInputTex.value = "";
     mInputAnswer.value = "";
     compileTex();
   }else{
     mInputTex.disabled = false;
     mInputAnswer.disabled = false;
+    mSaveButton.disabled = false;
   }
 }
 function loadTestId(){
@@ -38,4 +45,11 @@ function newQuestion(){
   var answer = mInputAnswer.value;
   var sel=select.selectedIndex;
   uploadQuestion(id,sel,question,answer);
+}
+function newTask(){
+  var options=select.options;
+  let newOption = new Option(options.length, "");
+  select.append(newOption);
+  newOption.selected = true;
+  loadNumber();
 }
---
+++ auth_init.js
@@ -0,0 +1,41 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function signIn(){
+  // firebase.auth().signInWithPopup(provider).then(function(result) {
+  //   // This gives you a Google Access Token. You can use it to access the Google API.
+  //   var token = result.credential.accessToken;
+  //   // The signed-in user info.
+  //   var user = result.user;
+  //   document.location.href = "main.html";
+  // }).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   var email = error.email;
+  //   // The firebase.auth.AuthCredential type that was used.
+  //   var credential = error.credential;
+  //   // ...
+  // });
+  // Using a redirect.
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token.
+    var token = result.credential.accessToken;
+  }
+  var user = result.user;
+});
+
+// Start a sign in process for an unauthenticated user.
+
+provider.addScope('profile');
+provider.addScope('email');
+firebase.auth().signInWithRedirect(provider);
+}
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    document.location.href = "index.html";
+  }).catch(function(error) {
+  // An error happened.
+  });
+}
